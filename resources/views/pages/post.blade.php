31b2e6c34afe8863b2296c63b886aa527c3666c5
chidokato <tuan.pn92@gmail.com>
2023-05-17 14:09:05 +0700

    sdf

--- resources/views/pages/post.blade.php
+++ resources/views/pages/post.blade.php
@@ -5,166 +5,102 @@
 @section('robots') index, follow @endsection
 @section('url'){{asset('')}}@endsection
 
+@section('css')
+<link href="frontend/content/Css/room-single.css" rel="stylesheet" />
+<link href="frontend/content/Css/blog.css" rel="stylesheet" />
+
+
+<style type="text/css">
+.booking_content h2{line-height: 1rem;margin-bottom: 10px;}
+.star-rating{margin-bottom: 10px}
+.star-rating i{ color: #ffc107 }
+.booking_icon{margin-top: 10px;}
+.row_item-img img {width: 100%;height: 165px;object-fit: cover;}
+.row_container{ margin-top: 40px }
+.header-page h1{ font-size: 1.5rem; border-bottom: 1px solid #ddd; padding-bottom: 10px; margin-bottom: 20px; }
+.header-page h1 span{ position: relative }
+.header-page h1 span:after{
+    position: absolute;
+    content: '';    background: #ffc107;
+    width: 100%;
+    height: 2px;
+    bottom: -12px;
+    left: 0;
+}
+.input_email input{
+    border: 1px solid #ddd;
+}
+.content h2{
+    font-size: 1.2rem
+}
+.content ul{ padding-left: 20px; }
+.content ul li{ list-style: circle; text-align: justify; }
+.content p{ text-align: justify; }
+</style>
+@endsection
+
 @section('content')
-<div id="page_wrapper" class="bg-light">
-@include('layout.header_page')
 
-<div class="full-row py-5" style="margin-top: 70px;">
-    <div class="container">
+<section class="main">
+    <div class="detail-blog__wrapper">
+        <div class="main_space"></div>
+    <div class="container mb-5">
         <div class="row">
-            <div class="col">
-                <h3 class="text-secondary">Single Blog</h3>
-                <nav aria-label="breadcrumb">
-                    <ol class="breadcrumb mb-0 bg-transparent p-0">
-                        <li class="breadcrumb-item"><a href="index-2.html">Home</a></li>
-                        <li class="breadcrumb-item"><a href="blog-grid-v1.html">Blog</a></li>
-                        <li class="breadcrumb-item active text-primary" aria-current="page">Single Blog</li>
-                    </ol>
-                </nav>
-            </div>
-        </div>
-    </div>
-</div>
-
-<div class="full-row pt-0">
-    <div class="container">
-        <div class="row">
-            <div class="col-xl-4 order-xl-2">
-                <div class="blog-sidebar widget-box-model">
-                    <!-- Search Field -->
-                    <div class="widget widget_search">
-                        <form role="search" method="get" class="search_form" action="http://localhost/axeman-wp/">
-                            <label>
-                            <span class="screen-reader-text">Search for:</span>
-                            <input type="search" class="search-field" placeholder="Search …" value="" name="s">
-                        </label>
-                            <input type="submit" class="search-submit" value="Search">
-                        </form>
-                    </div>
-                    <!-- Category Field -->
-                    <div class="widget widget_categories">
-                        <h5 class="widget-title mb-3">Categories</h5>
-                        <ul>
-                            <li class="cat-item cat-item-3"><a href="#">Apartment</a> (23)</li>
-                            <li class="cat-item cat-item-2"><a href="#">Condo</a> (10)</li>
-                            <li class="cat-item cat-item-2"><a href="#">Family House</a> (09)</li>
-                            <li class="cat-item cat-item-2"><a href="#">Modern Villa</a> (35)</li>
-                            <li class="cat-item cat-item-2"><a href="#">Town House</a> (05)</li>
-                        </ul>
-                    </div>
-                    
-
-                    <!--============== Recent Property Widget Start ==============-->
-                    <div class="widget widget_recent_property">
-                        <h5 class="text-secondary mb-4">{{$post->name}}</h5>
-                        <ul>
-                            <li>
-                                <img src="assets/images/thumbnaillist/01.jpg" alt="">
-                                <div class="thumb-body">
-                                    <h6 class="listing-title"><a href="property-single-1.html">Nirala Appartment</a></h6>
-                                    <span class="listing-price">$3200<small>( Monthly )</small></span>
-                                    <ul class="d-flex quantity font-fifteen">
-                                        <li title="Area"><span><i class="fa-solid fa-vector-square"></i></span>6500 Sqft</li>
-
-                                    </ul>
-                                </div>
-                            </li>
-                            <li>
-                                <img src="assets/images/thumbnaillist/02.jpg" alt="">
-                                <div class="thumb-body">
-                                    <h6 class="listing-title"><a href="property-single-1.html">Condo House</a></h6>
-                                    <span class="listing-price">$11500<small>( Monthly )</small></span>
-                                    <ul class="d-flex quantity font-fifteen">
-                                        <li title="Area"><span><i class="fa-solid fa-vector-square"></i></span>2200 Sqft</li>
-                                    </ul>
-                                </div>
-                            </li>
-                            <li>
-                                <img src="assets/images/thumbnaillist/03.jpg" alt="">
-                                <div class="thumb-body">
-                                    <h6 class="listing-title"><a href="property-single-1.html">Luxury Condos</a></h6>
-                                    <span class="listing-price">$17000<small>( Monthly )</small></span>
-                                    <ul class="d-flex quantity font-fifteen">
-                                        <li title="Area"><span><i class="fa-solid fa-vector-square"></i></span>3500 Sqft</li>
-                                    </ul>
-                                </div>
-                            </li>
-                            <li>
-                                <img src="assets/images/thumbnaillist/04.jpg" alt="">
-                                <div class="thumb-body">
-                                    <h6 class="listing-title"><a href="property-single-1.html">Small Appartment</a></h6>
-                                    <span class="listing-price">$5200<small>( Monthly )</small></span>
-                                    <ul class="d-flex quantity font-fifteen">
-                                        <li title="Area"><span><i class="fa-solid fa-vector-square"></i></span>1200 Sqft</li>
-                                    </ul>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-                    
+           <div class="col-lg-8">
+               <div class="detail-blog">
+                <div class="header-page">
+                    <h1><span>{{$post->name}}</span></h1>
                 </div>
-            </div>
-            <div class="col-xl-8 order-xl-1 sm-mb-30">
-                <div class="single-post border summary bg-white p-30 mb-30">
-                    <div class="single-post-title">
-                        <span class="d-inline-block text-primary">{{$post->CategoryTranslation->name}}</span>
-                        <h4 class="mb-3 text-secondary">{{$post->name}}</h4>
-                        <div class="post-meta list-color-general mb-4">
-                            <a href="#"><i class="flaticon-user-silhouette flat-mini"></i> <span>By Admin</span></a>
-                            <a href="#"><i class="flaticon-calendar flat-mini"></i> <span>Sep 1, 2019</span></a>
-                            <a href="#"><i class="flaticon-comments flat-mini"></i> <span>02</span></a>
-                            <a href="#"><i class="flaticon-like flat-mini"></i> <span>30</span></a>
-                            <a href="#"><i class="flaticon-eye-1 flat-mini"></i> <span>731</span></a>
-                            <span><i class="flaticon-document flat-mini"></i> <a href="#"><span>General</span></a>,<a href="#"><span>Media</span></a></span>
-                        </div>
+                   <div class="content">
+                       {!! $post->content !!}
+                   </div>
+               </div>
+           </div>
+           <div class="col-lg-4">
+               <div class="reservation_hotel">
+                    <div class="reservation_title">
+                        <h5 class="reservation_title-name">
+                            Tìm kiếm
+                        </h5>
                     </div>
-                    <div class="post-image">
-                        <img src="data/news/{{$post->post->img}}" alt="Image not found!">
-                    </div>
-                    <div class="post-content pt-4 mb-5">
-                        {!! $post->content !!}
-                    </div>
-                    <!-- <div class="tagcloud">
-                        <h6 class="mb-3">Tags:</h6>
-                        <ul>
-                            <li><a href="#">general</a></li>
-                            <li><a href="#">videos</a></li>
-                            <li><a href="#">media</a></li>
-                            <li><a href="#">web</a></li>
-                            <li><a href="#">parallax</a></li>
-                            <li><a href="#">ecommerce</a></li>
-                            <li><a href="#">t-shirt</a></li>
-                            <li><a href="#">women</a></li>
-                            <li><a href="#">trade</a></li>
-                            <li><a href="#">animation</a></li>
-                            <li><a href="#">theme</a></li>
-                        </ul>
-                    </div> -->
-                    <div class="share-post border-0 px-0 d-flex mt-5">
-                        <span class="py-10"><b>Share:</b></span>
-                        <div class="media-widget-round-white-primary-shadow">
-                            <a href="#"><i class="fab fa-facebook-f"></i></a>
-                            <a href="#"><i class="fab fa-twitter"></i></a>
-                            <a href="#"><i class="fab fa-linkedin-in"></i></a>
-                            <a href="#"><i class="fab fa-google-plus-g"></i></a>
-                            <a href="#"><i class="fab fa-pinterest-p"></i></a>
+                    <div class="hotel_content">
+                        <div class="input_email">
+                            <input type="text" placeholder="Nhập từ khóa tìm kiếm">
                         </div>
                     </div>
                 </div>
 
-                
-
-                
-            </div>
+                <div class="block_text">
+                    <div class="block_title">
+                        <h2 class="block_title-name">Bài đăng gần đây</h2>
+                    </div>
+                    <div class="block_content">
+                            
+                        <a href="/tin-tucs/3137/ban-lao-chai-ta-van">
+                            <div class="blog-item">
+                                <div class="blog-item-img">
+                                    <img src="frontend/files/images/news/kham-pha-ban-lao-chai-ta-van-khi-di-du-lich-sapa-3.jpg" alt="ban-lao-chai-ta-van">
+                                </div>
+                                <div class="blog-item-text" style="">
+                                    <div class="item-text-title">
+                                Bản Lao Chải &amp; Tả Van
+                                    </div>
+                                    <div class="item-text-date"><em>Tháng Năm 12, 2022</em></div>
+                                </div>
+                            </div>
+                        </a>
+                        <hr style="margin:0">
+                            
+                    </div>
+                </div>
+           </div>
         </div>
+        
     </div>
-</div>
+    </div>
 
+</section>
 
-
-
-@include('layout.footer')
-</div>
 @endsection
 @section('script')
 
