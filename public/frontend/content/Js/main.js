00bf5a44d4cd65e454c32268db3f3b89b8290e53
chidokato <tuan.pn92@gmail.com>
2023-06-29 11:16:19 +0700

    sdsdf

--- public/frontend/content/Js/main.js
+++ public/frontend/content/Js/main.js
@@ -244,7 +244,7 @@ $('.owlCarousel123').owlCarousel({
     responsiveClass: false,
     responsive:{
         0:{
-            items:1
+            items:2
         },
         600:{
             items:3
